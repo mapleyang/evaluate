c14efb9140df3fd414745315e3b3cf070ebb5a2d
mappleyang <maple.yang@gravityhealth.com.cn>
2017-11-29 16:17:45 +0800

    home

--- src/containers/component/home.js
+++ src/containers/component/home.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react'
 import './index.scss'
 import classnames from "classnames";
+import { Carousel, Flex } from 'antd-mobile';
 
 
 class Home extends Component {
   constructor(props, context) {
     super(props)
     this.state = {
+      data: ['', '', ''],
     }
   }
 
@@ -20,6 +22,34 @@ class Home extends Component {
       <div className="home">
         <div className="header">
           保险产品
+        </div>
+        <div className="pro-list">
+          <Carousel
+            className="pro-carousel"
+            autoplay={false}
+            infinite
+            dots={false}
+            selectedIndex={1}>
+            {this.state.data.map(ii => (
+              <a key={ii}>
+                <img
+                  src="./beginning.jpg" alt=""/>
+              </a>
+            ))}
+          </Carousel>
+          <div className="item-title">
+            保险推荐
+          </div>
+          <div className="insurance-list">
+            {this.state.data.map((el, index) => {
+              return <div>
+                <Flex>
+                  <Flex.Item className="insurance-img"><img src={"./insurance" + index + ".jpg"} /></Flex.Item>
+                  <Flex.Item>保险{index}</Flex.Item>
+                </Flex>
+              </div>
+            })}
+          </div>
         </div>
       </div>
     );
