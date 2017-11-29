356adad6f8082d6f185ea812343121a54cb88464
mappleyang <maple.yang@gravityhealth.com.cn>
2017-11-29 17:32:25 +0800

    init

--- src/containers/component/home.js
+++ src/containers/component/home.js
@@ -9,6 +9,22 @@ class Home extends Component {
     super(props)
     this.state = {
       data: ['', '', ''],
+      insurance: [{
+        title: '太健康·百万全家桶',
+        age: "90天至60周岁",
+        time: "1年",
+        content: "住院医疗保险金 100万/人特许医疗 可选赔付比例 100%"
+      },{
+        title: "心安•怡住院医疗保险",
+        age: "0至60周岁",
+        time: "1年",
+        content: "产品升级，病种由原60中提升至88种、续保年龄延长至80周岁"
+      },{
+        title: "少儿超能宝两全保险",
+        age: "0-17周岁",
+        time: "30年",
+        content: "身故或全残保险金 10万元/份 重大疾病保险金"
+      }]
     }
   }
 
@@ -41,11 +57,21 @@ class Home extends Component {
             保险推荐
           </div>
           <div className="insurance-list">
-            {this.state.data.map((el, index) => {
+            {this.state.insurance.map((el, index) => {
               return <div>
-                <Flex>
+                <Flex className="insurance-info">
                   <Flex.Item className="insurance-img"><img src={"./insurance" + index + ".jpg"} /></Flex.Item>
-                  <Flex.Item>保险{index}</Flex.Item>
+                  <Flex.Item className="insurance-intr">
+                    <div className="insurance-title">{el.title}</div>
+                    <div className="insurance-item">
+                      <span className="insurance-item-title">承保年龄</span>
+                      <span>{el.age}</span>
+                    </div>
+                    <div className="insurance-item">
+                      <span className="insurance-item-title">主要保障</span>
+                      <span>{el.content}</span>
+                    </div>
+                  </Flex.Item>
                 </Flex>
               </div>
             })}
